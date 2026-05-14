d1cbc93fd3b545d4ce23cdf6925c2c4224dbb1ad
Horacio Sierra <perez2499@gmail.com>
2026-05-14 16:56:58 -0400

    Add pixel cat turn around animation

--- src/components/PixelCat.jsx
+++ src/components/PixelCat.jsx
@@ -4,10 +4,12 @@ function PixelCat() {
   // Debug: show one static frame
   return (
     <div className="cat-pet">
-      <div className="cat-scale">
-        <div className="pixel-cat"></div>
-      </div>
+       <div className="cat-flip">
+        <div className="cat-scale">
+      <div className="pixel-cat"></div>
     </div>
+  </div>
+</div>
   );
 }
 
